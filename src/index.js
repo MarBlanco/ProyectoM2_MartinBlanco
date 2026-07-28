3cf592bbf765d066572e12bfd2eae6a2d082b6f8
Martin Blanco <muawallace@hotmail.com>
2026-07-28 08:55:58 -0300

    feat: estructura inicial del servidor funcionando

--- src/index.js
+++ src/index.js
@@ -1,1 +1,15 @@
-require("./config/db");
+const express = require("express");
+
+const app = express();
+
+app.use(express.json());
+
+app.get("/", (req, res) => {
+    res.send("Servidor funcionando");
+});
+
+const PORT = 3000;
+
+app.listen(PORT, () => {
+    console.log(`Servidor escuchando en el puerto ${PORT}`);
+});
