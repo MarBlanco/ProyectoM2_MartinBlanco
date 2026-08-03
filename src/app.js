43e80ac13e6c512df820b7fb842369308e7fa012
Martin Blanco <muawallace@hotmail.com>
2026-08-03 18:48:44 -0300

    doc: agregar documentacion swagger

--- src/app.js
+++ src/app.js
@@ -5,9 +5,13 @@ const authorsRoutes = require("./routes/authorsRoutes");
 const postsRoutes = require("./routes/postsRoutes");
 const errorHandler = require("./middlewares/errorHandler");
 
+const { swaggerUi, swaggerDocument } = require("./docs/swagger");
+
 const app = express();
 
 app.use(express.json());
+
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
 app.use("/authors", authorsRoutes);
 app.use("/posts", postsRoutes);
